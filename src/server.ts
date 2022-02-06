d050553a7a476a65e47c723cf08ee54651bd435a
Bruno Martins <brunomart97@hotmail.com>
2022-02-05 21:01:20 -0300

    Database configuration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}!`);
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
